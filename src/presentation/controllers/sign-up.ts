c650be184f5a2fcccf9eeb797e22d49dbf23bb60
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-01-24 16:05:06 -0200

    refactor(SignUpController): simplify validation code

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -5,12 +5,12 @@ import badRequest from '../helpers/http-helpers';
 
 export default class SignUpController {
   public handle(httpRequest: IHttpRequest): IHttpRespose {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
+    const requiredFields = ['name', 'email'];
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'));
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
 
     return {
