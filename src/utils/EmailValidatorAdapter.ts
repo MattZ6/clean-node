c34fba6d9e229094e5c3726c5968a253756eeeaf
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-01-27 13:45:32 -0200

    feat(EmailValidatorAdapter): ensure return true if validator returns true

--- src/utils/EmailValidatorAdapter.ts
+++ src/utils/EmailValidatorAdapter.ts
@@ -1,7 +1,9 @@
+import validator from 'validator';
+
 import IEmailValidator from '../presentation/protocols/IEmailValidator';
 
 export default class EmailValidatorAdapter implements IEmailValidator {
-  public isValid(_: string): boolean {
-    return false;
+  public isValid(email: string): boolean {
+    return validator.isEmail(email);
   }
 }
