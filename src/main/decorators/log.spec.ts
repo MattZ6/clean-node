402e99490a0dcf84a67f2e73aaa6f089bf141991
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-01 20:54:38 -0200

    test(LogControllerDecorator): ensure calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,42 @@
+import { ok } from '../../presentation/helpers/http';
+import {
+  IController,
+  IHttpRequest,
+  IHttpRespose,
+} from '../../presentation/protocols';
+
+import { LogControllerDecorator } from './log';
+
+class ControllerStub implements IController {
+  async handle(_: IHttpRequest): Promise<IHttpRespose> {
+    return ok();
+  }
+}
+
+let controllerStub: ControllerStub;
+let systemUnderTest: LogControllerDecorator;
+
+describe('LogControllerDecorator', () => {
+  beforeEach(() => {
+    controllerStub = new ControllerStub();
+
+    systemUnderTest = new LogControllerDecorator(controllerStub);
+  });
+
+  it('should call controller handle method', async () => {
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+
+    const request: IHttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    await systemUnderTest.handle(request);
+
+    expect(handleSpy).toHaveBeenCalledWith(request);
+  });
+});
