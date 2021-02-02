f10feeec609cc3361048e769f230b891d0574b25
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-01 22:42:37 -0200

    feat(SignInController): ensure return 400 if an invalid email is provided

--- src/presentation/controllers/sign-in/SignInController.ts
+++ src/presentation/controllers/sign-in/SignInController.ts
@@ -1,16 +1,26 @@
-import { MissingParamError } from '../../errors';
+import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http';
 import { IController, IHttpRequest, IHttpRespose } from '../../protocols';
-import { ISignInDataDTO } from './SignInController.types';
+import { IEmailValidator } from '../../protocols/IEmailValidator';
 
 export class SignInController implements IController {
-  async handle(request: IHttpRequest<ISignInDataDTO>): Promise<IHttpRespose> {
+  constructor(private readonly emailValidator: IEmailValidator) {}
+
+  async handle(request: IHttpRequest): Promise<IHttpRespose> {
     const requiredFields = ['email', 'password'];
 
     for (const field of requiredFields) {
       if (!(request.body as any)[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const { email } = request.body;
+
+    const isValidEmail = this.emailValidator.isValid(email);
+
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'));
     }
 
     return {} as IHttpRespose;
--- src/presentation/controllers/sign-in/SignInController.types.ts
+++
@@ -1,4 +0,0 @@
-export interface ISignInDataDTO {
-  email?: string;
-  password?: string;
-}
