9c917e480a55b6343c0e09bfac0efed047e868ae
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-03 21:25:41 -0200

    test(CompareFieldsValidation): add tests

---
+++ src/presentation/helpers/validators/CompareFieldsValidation.spec.ts
@@ -0,0 +1,34 @@
+import { InvalidParamError } from '../../errors';
+import { CompareFieldsValidation } from './CompareFieldsValidation';
+
+let systemUnderTest: CompareFieldsValidation;
+
+describe('CompareFieldsValidation', () => {
+  const FIELD_NAME = 'field';
+  const FIELD_TO_COMPARE_NAME = 'field_to_compare';
+
+  beforeEach(() => {
+    systemUnderTest = new CompareFieldsValidation(
+      FIELD_NAME,
+      FIELD_TO_COMPARE_NAME
+    );
+  });
+
+  it('should return a InvalidParamError if validation fails', () => {
+    const result = systemUnderTest.validate({
+      [FIELD_NAME]: 'any_value',
+      [FIELD_TO_COMPARE_NAME]: 'another_value',
+    });
+
+    expect(result).toEqual(new InvalidParamError(FIELD_NAME));
+  });
+
+  it('should return null if validation succeeds', () => {
+    const result = systemUnderTest.validate({
+      [FIELD_NAME]: 'equal_value',
+      [FIELD_TO_COMPARE_NAME]: 'equal_value',
+    });
+
+    expect(result).toBeNull();
+  });
+});
