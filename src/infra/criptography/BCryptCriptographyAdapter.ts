0e13e7bb46b56e0cdfce5850730aecb30b5785c0
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-04 22:50:52 -0200

    feat(BCryptCriptographyAdapter): add compare implementation

--- src/infra/criptography/BCryptCriptographyAdapter.ts
+++ src/infra/criptography/BCryptCriptographyAdapter.ts
@@ -1,13 +1,18 @@
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 
 import { IHasher } from '../../data/protocols/criptography/IHasher';
+import { IHashComparer } from '../../data/protocols/criptography/IHashComparer';
 
-export class BCryptCriptographyAdapter implements IHasher {
+export class BCryptCriptographyAdapter implements IHasher, IHashComparer {
   constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
     const hashedValue = await hash(value, this.salt);
 
     return hashedValue;
+  }
+
+  async compare(value: string, hashedValue: string): Promise<boolean> {
+    return compare(value, hashedValue);
   }
 }
