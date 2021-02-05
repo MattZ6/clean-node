cd9c762f7bee096a32b12c8af429c985c28b76bc
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-04 22:24:53 -0200

    refactor(DbAuthentication): move imports to a single file

--- src/data/usecases/authentication/DbAuthentication.protocols.ts
+++ src/data/usecases/authentication/DbAuthentication.protocols.ts
@@ -1,3 +1,4 @@
+export * from '../../../domain/models/IAccount';
 export * from '../../../domain/usecases/IAuthentication';
 export * from '../../protocols/db/IGetAccountByEmailRepository';
 export * from '../../protocols/db/IUpdateAccessTokenRepository';
--- src/data/usecases/authentication/DbAuthentication.spec.ts
+++ src/data/usecases/authentication/DbAuthentication.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-classes-per-file */
-import { IAccountModel } from '../../../domain/models/IAccount';
 import {
+  IAccountModel,
   IGetAccountByEmailRepository,
   IHashComparer,
   ITokenGenerator,
