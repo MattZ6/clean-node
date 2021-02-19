2abad8dd873473499c358598eaf7804ab9f88b83
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-18 22:00:48 -0200

    test(MongoAccountRepository): ensure it updates account access token on updateAccessToken success

--- src/infra/db/mongodb/account-repository/MongoAccountRepository.spec.ts
+++ src/infra/db/mongodb/account-repository/MongoAccountRepository.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { Collection } from 'mongodb';
 
 import { mongoHelper } from '../helpers/mongo-helper';
@@ -67,5 +68,29 @@ describe('MongoAccountRepository', () => {
     expect(account?.name).toBe(name);
     expect(account?.email).toBe(email);
     expect(account?.password).toBe(password);
+  });
+
+  it('should update the account access token on updateAccessToken success', async () => {
+    const response = await accountsCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    const [user] = response.ops;
+
+    expect(user.accessToken).toBeFalsy();
+
+    const updatedToken = 'updated_token';
+
+    await systemUnderTest.updateAccessToken({
+      accountId: user._id,
+      accessToken: updatedToken,
+    });
+
+    const account = await accountsCollection.findOne({ _id: user._id });
+
+    expect(account).toBeTruthy();
+    expect(account?.accessToken).toBe(updatedToken);
   });
 });
