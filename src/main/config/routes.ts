e750b5c31a231ab4fa52f140797bdd043e5ffaad
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-20 11:33:00 -0200

    refactor: change routes to use file system instead fast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,17 @@
 import { Express, Router } from 'express';
-import { sync } from 'fast-glob';
+import { join } from 'path';
+import { readdirSync } from 'fs';
 
 export default (app: Express): void => {
   const router = Router();
 
   app.use('/api', router);
 
-  sync('**/src/main/routes/*.routes.ts').forEach(async filePath =>
-    (await import(`../../../${filePath}`)).default(router)
-  );
+  const dir = join(__dirname, '..', 'routes');
+
+  readdirSync(dir)
+    .filter(fileName => !fileName.includes('.test.'))
+    .map(async filename => {
+      (await import(join(dir, filename))).default(router);
+    });
 };
--- src/main/server.ts
+++ src/main/server.ts
@@ -10,7 +10,7 @@ mongoHelper
     const app = (await import('./config/app')).default;
 
     app.listen(env.port, () =>
-      console.log(`Server running at http:localhost:${env.port}`)
+      console.log(`Server running at http//:localhost:${env.port}`)
     );
   })
   .catch(err => {
