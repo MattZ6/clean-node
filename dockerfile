cfbde156e0eb283b070a732864d2fc58642c853d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-22 20:00:29 -0300

    chore: add docker-compose file

--- dockerfile
+++ dockerfile
@@ -8,11 +8,3 @@ COPY ./package.json .
 
 # Instala somente as dependências de produção
 RUN yarn --prod
-
-# Copia a pasta dist para a pasta ./dist do container
-COPY ./dist ./dist
-
-# Deixa o container acessível a partir de uma porta específica
-EXPOSE 3333
-
-CMD yarn start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export default {
   jwtSecret: process.env.SECRET || 'SECRET',
 
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
 
   port: process.env.PORT || 3333,
 };
